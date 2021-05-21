b40059edc85c4aa7e8195caa1259bf79cb5651af
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-21 14:31:12 +0100

    room 1 has song and guest pass

--- src/room.py
+++ src/room.py
@@ -1,4 +1,6 @@
 class Room:
-    def __init__(self, name):
+    def __init__(self, name, guest_list, song_list):
         self.name = name
-        # self.song_list = song_list
+        self.guest_list = guest_list
+        self.song_list = song_list
+        
--- tests/room_test.py
+++ tests/room_test.py
@@ -5,10 +5,25 @@ from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room_1 = Room("Room 1")
-        self.room_2 = Room("Room 2")
+        self.guest_1 = Guest("Tristan")
+        self.song_1 = Song("Rex Orange County", "New House")
+        self.room_1 = Room("Room 1", [self.guest_1.name], [
+            {"artist_name": self.song_1.artist_name,
+            "song_name": self.song_1.song_name}
+        ])
+    
 
     def test_room_1_has_name(self):
         expected = "Room 1"
         actual = self.room_1.name
+        self.assertEqual(expected, actual)
+    
+    def test_room_1_has_song_in_room(self):
+        expected = [{"artist_name": "Rex Orange County", "song_name": "New House"}]
+        actual = self.room_1.song_list
+        self.assertEqual(expected, actual)
+
+    def test_room_1_has_guest_in_room(self):
+        expected = ["Tristan"]
+        actual = self.room_1.guest_list
         self.assertEqual(expected, actual)
