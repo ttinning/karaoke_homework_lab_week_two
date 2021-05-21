c01d2771101b829cf0e1c0584552d04caeb890f1
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-21 13:52:25 +0100

    2nd guest created, test 2 pass

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,9 +3,15 @@ from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest("Tristan")
+        self.guest_1 = Guest("Tristan")
+        self.guest_2 = Guest("Clayre")
 
-    def test_guest_has_name(self):
+    def test_guest_1_has_name(self):
         expected = "Tristan"
-        actual = self.guest.name
+        actual = self.guest_1.name
+        self.assertEqual(expected, actual)
+
+    def test_guest_2_has_name(self):
+        expected = "Clayre"
+        actual = self.guest_2.name
         self.assertEqual(expected, actual)
