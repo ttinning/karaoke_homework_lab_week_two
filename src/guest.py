6df4f170a91315853439e09938e678530d1fe383
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-21 13:48:42 +0100

    Guest class created, test 1 pass

---
+++ src/guest.py
@@ -0,0 +1,4 @@
+class Guest:
+    def __init__(self, name):
+        self.name = name
+        
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,1 +1,11 @@
+import unittest
+from src.guest import Guest
 
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest = Guest("Tristan")
+
+    def test_guest_has_name(self):
+        expected = "Tristan"
+        actual = self.guest.name
+        self.assertEqual(expected, actual)
